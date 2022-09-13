d918a17e5a02e133d622c196921a9893036c7138
18336020768 <1423044909@qq.com>
2022-09-13 19:04:52 +0800

    test01

---
+++ publiccode/src/main/java/com/zh/publiccode/PublicCodeApplication.java
@@ -0,0 +1,13 @@
+package com.zh.publiccode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicCodeApplication.class, args);
+    }
+
+}
