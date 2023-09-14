0c3f47d1a79370db0f2c087e30f00d5f319b512f
zhJAva <56397661+zhCodes@users.noreply.github.com>
2023-09-14 16:54:31 +0800

    Delete z-code directory

--- z-code/src/main/java/com/zh/code/service/impl/ZUserService.java
+++
@@ -1,9 +0,0 @@
-package com.zh.code.service.impl;
-
-/**
- * @Date: 2023/4/14 13:35
- * @Author: Zhanghuihui
- * @Description:
- */
-public interface ZUserService {
-}
--- z-code/src/main/java/com/zh/code/service/impl/ZUserServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.zh.code.service.impl;
-
-/**
- * @Date: 2023/4/14 13:36
- * @Author: Zhanghuihui
- * @Description:
- */
-public class ZUserServiceImpl{
-}
